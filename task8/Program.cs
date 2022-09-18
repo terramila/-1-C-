dc802963b0023e68d6b7f212f54c200296ce7047
tarana <beshirova@hotmail.com>
2022-09-19 02:22:12 +0300

    задача8

--- task8/Program.cs
+++ task8/Program.cs
@@ -3,14 +3,8 @@
 5 -> 2, 4
 8 -> 2, 4, 6, 8 */
 
-
-
 Console.WriteLine("Введите число:  ");
 int num = Convert.ToInt32(Console.ReadLine());
-int min = 0;
-while ( min < num )
-{
-Console.Write(($" {min += 2}  "));
-} 
-
+for (int i = 2; i <= num; i += 2)
+ Console.Write($" {i} ");
 
